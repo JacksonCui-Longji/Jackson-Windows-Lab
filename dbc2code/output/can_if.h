e892bf4ad29e50d79c2cef7b3671e482ee043b83
JacksonCui-Longji <aj1340468983@gmail.com>
2026-07-21 15:18:33 +1000

    finish dbc2code

--- dbc2code/output/can_if.h
+++ dbc2code/output/can_if.h
@@ -15,6 +15,7 @@ typedef enum ByteOrder_t {
 typedef enum CanSignalType_t {
     CAN_SIG_TYPE_PERIOD = 0,
     CAN_SIG_TYPE_ONCHANGE,
+    CAN_SIG_TYPE_PERIOD_ONCHANGE,
 } CanSignalType;
 
 typedef enum CanIfRet_t {
@@ -24,8 +25,8 @@ typedef enum CanIfRet_t {
 
 typedef struct SignalInfo_t {
     uint8_t  message_index;
-    uint16_t start_bit;      // CAN FD max is 511, uint8_t can't hold it, use uint16_t 
-    uint8_t length;
+    uint16_t start_bit;      // CAN FD max is 511, uint8_t can't hold it, use uint16_t
+    uint8_t  length;
     ByteOrder byte_order;
     bool is_signed;
     float factor;
@@ -40,18 +41,19 @@ typedef struct MessageBuffer_t {
     uint8_t  data[CAN_FD_MAX_DLC];
 } MessageBuffer;
 
-typedef CanIfRet (*signalcallback)(CanSignalId sig_id, float value);
+typedef CanIfRet (*SignalCallback)(CanSignalId sig_id, float value);
 
-typedef struct SubScriberInfo_t{
+typedef struct SubscriberInfo_t {
     CanSignalId sig_id;
     CanSignalType sub_type;
-    signalcallback callback;
-}SubScriberInfo;
+    uint32_t period_ms;
+    SignalCallback callback;
+} SubscriberInfo;
 
 CanIfRet GetSignalValue(CanSignalId sig_id, float* out_value);
 CanIfRet SetSignalValue(CanSignalId sig_id, float physical_value);
-CanIfRet SubscribeCanSig(SubScriberInfo *info, uint32_t num);
-
+CanIfRet SubscribeCanSig(SubscriberInfo *info, uint32_t num);
+CanIfRet PublishSignal(CanSignalId sig_id);
 CanIfRet UpdateCanFrame(uint32_t message_id, const uint8_t* data, uint8_t dlc);
 
 #endif // CAN_IF_H
